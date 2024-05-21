8d9c9fa474c660b0a05300e9fd559e91f3fec78e
Woodly Boursiquot <127155151+WoodlyB@users.noreply.github.com>
2024-05-21 12:25:54 -0400

    Finished the CSS and started the JavaScript functions
    I finished the CSS animation for when the user clicks the previous button and next button. Started the JavaScript functions to make it work.

--- slider_v1/js/app.js
+++ slider_v1/js/app.js
@@ -1,1 +1,52 @@
-console.log("Helo App.js ");
+document.addEventListener('DOMContentLoaded', function() {
+// Select the arrow btn 
+let nextDom = document.getElementById("next");
+let prevDom = document.getElementById("prev"); 
+
+// Select element inside the Carousel 
+let carourelDom = document.querySelector(".carousel") ;
+
+let listItemDom = document.querySelector(" .carousel .list")
+let thumbnailDom = document.querySelector(" .carousel .thumbnail")
+
+
+nextDom.onclick = function(){
+    showSlider("next");
+}
+
+
+prevDom.onclick = function(){
+    showSlider("prev");
+}
+
+
+
+let timeRunning =3000 ;
+let runTimeOut;
+
+function showSlider(type){
+    let itemSlider = document.querySelectorAll(".carousel .list .item ");
+    let itemThumbnail = document.querySelectorAll(".carousel .thumbnail .item ")
+
+    if (type === "next") {
+      
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        carourelDom.classList.add("next"); 
+    }else{
+        console.log("else");
+            let positionLastItem = itemSlider.length -1;
+            listItemDom.prepend(itemSlider[positionLastItem]);
+            thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+            carourelDom.classList.add("prev"); 
+
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carourelDom.classList.remove("next");
+        carourelDom.classList.remove("prev");
+    }, timeRunning)
+};
+
+
+});
